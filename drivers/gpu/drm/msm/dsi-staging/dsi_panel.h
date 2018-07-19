3b287e7050efb33f3a268f7b0be4fb509624601e
Danny Baumann <dannybaumann@web.de>
2018-07-19 09:59:55 +0200

    drm: msm: Add support for display modes.
    Change-Id: I0e1f2bfa2348e5c94d67147c9fec4b042b0cabf4

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -148,6 +148,15 @@ enum dsi_panel_type {
 	DSI_PANEL_TYPE_MAX,
 };
 
+enum dsi_panel_display_mode {
+	DISPLAY_MODE_DEFAULT,
+	DISPLAY_MODE_SRGB,
+	DISPLAY_MODE_DCI_P3,
+	DISPLAY_MODE_NIGHT,
+	DISPLAY_MODE_ONEPLUS,
+	DISPLAY_MODE_ADAPTION
+};
+
 struct dsi_panel {
 	const char *name;
 	enum dsi_panel_type type;
@@ -178,6 +187,7 @@ struct dsi_panel {
 	struct drm_panel_esd_config esd_config;
 
 	int hbm_mode;
+	enum dsi_panel_display_mode display_mode;
 
 	bool lp11_init;
 	bool ulps_enabled;
@@ -272,6 +282,8 @@ int dsi_panel_unprepare(struct dsi_panel *panel);
 int dsi_panel_post_unprepare(struct dsi_panel *panel);
 
 int dsi_panel_apply_hbm_mode(struct dsi_panel *panel);
+
+int dsi_panel_apply_display_mode(struct dsi_panel *panel);
 
 int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl);
 
