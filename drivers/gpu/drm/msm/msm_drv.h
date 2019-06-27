4b35a3c91c8fc3afeaa8b1f5666e9498f52cfcee
LuK1337 <priv.luk@gmail.com>
2019-06-27 12:30:23 +0200

    drm: msm: Import FOD changes from OnePlus 6T
    Change-Id: I0152542579d8bcd3160f1a4b999797a84873f7c8

--- drivers/gpu/drm/msm/msm_drv.h
+++ drivers/gpu/drm/msm/msm_drv.h
@@ -162,6 +162,7 @@ enum msm_mdp_crtc_property {
 
 	CRTC_PROP_ENABLE_SUI_ENHANCEMENT,
 	CRTC_PROP_IDLE_PC_STATE,
+	CRTC_PROP_CUSTOM,
 
 	/* total # of properties */
 	CRTC_PROP_COUNT
@@ -196,6 +197,8 @@ enum msm_mdp_conn_property {
 	CONNECTOR_PROP_AUTOREFRESH,
 	CONNECTOR_PROP_LP,
 	CONNECTOR_PROP_FB_TRANSLATION_MODE,
+
+	CONNECTOR_PROP_CUSTOM,
 
 	/* total # of properties */
 	CONNECTOR_PROP_COUNT
@@ -629,6 +632,9 @@ struct msm_drm_private {
 
 	/* update the flag when msm driver receives shutdown notification */
 	bool shutdown_in_progress;
+
+	/* commit end time */
+	ktime_t commit_end_time;
 };
 
 /* get struct msm_kms * from drm_device * */
