359f4acaf9c34f9747cfee0dc851f648ff2caa80
LuK1337 <priv.luk@gmail.com>
2021-09-08 23:22:29 +0200

    pstore: Import changes from oneplus/SDM845_R_11.0
    Change-Id: I5a368e6939e620f1418f1116a8a024b21930c9ed

--- include/linux/pstore.h
+++ include/linux/pstore.h
@@ -26,6 +26,7 @@
 #include <linux/errno.h>
 #include <linux/kmsg_dump.h>
 #include <linux/mutex.h>
+#include <linux/pstore_ram.h>
 #include <linux/spinlock.h>
 #include <linux/time.h>
 #include <linux/types.h>
@@ -42,6 +43,7 @@ enum pstore_type_id {
 	PSTORE_TYPE_PPC_COMMON	= 6,
 	PSTORE_TYPE_PMSG	= 7,
 	PSTORE_TYPE_PPC_OPAL	= 8,
+	PSTORE_TYPE_DEVICE_INFO	= 9,
 	PSTORE_TYPE_UNKNOWN	= 255
 };
 
@@ -89,4 +91,34 @@ extern int pstore_register(struct pstore_info *);
 extern void pstore_unregister(struct pstore_info *);
 extern bool pstore_cannot_block_path(enum kmsg_dump_reason reason);
 
+struct ramoops_context {
+	struct persistent_ram_zone **przs;
+	struct persistent_ram_zone *cprz;
+	struct persistent_ram_zone *fprz;
+	struct persistent_ram_zone *mprz;
+	struct persistent_ram_zone *dprz;
+	phys_addr_t phys_addr;
+	unsigned long size;
+	unsigned int memtype;
+	size_t record_size;
+	size_t console_size;
+	size_t ftrace_size;
+	size_t pmsg_size;
+	size_t device_info_size;
+	int dump_oops;
+	struct persistent_ram_ecc_info ecc_info;
+	unsigned int max_dump_cnt;
+	unsigned int dump_write_cnt;
+	/* _read_cnt need clear on ramoops_pstore_open */
+	unsigned int dump_read_cnt;
+	unsigned int console_read_cnt;
+	unsigned int ftrace_read_cnt;
+	unsigned int pmsg_read_cnt;
+	unsigned int device_info_read_cnt;
+	struct pstore_info pstore;
+};
+
+void save_dump_reason_to_device_info(char *buf);
+void save_tz_dump_reason_to_device_info(char *buf);
+void save_modem_dump_reason_to_device_info(char *buf);
 #endif /*_LINUX_PSTORE_H*/
