60de99c9b5627f530ca5a347bd020c8545589342
Tim Schumacher <timschumi@gmx.de>
2022-04-03 14:03:58 +0200

    drivers: drm: Import OnePlus DC dimming code
    Change-Id: I6664a3f5d759b28a401476bd464616d0502174cf

--- drivers/gpu/drm/msm/sde/sde_encoder.c
+++ drivers/gpu/drm/msm/sde/sde_encoder.c
@@ -3835,7 +3835,7 @@ void sde_encoder_trigger_kickoff_pending(struct drm_encoder *drm_enc)
 	}
 }
 
-extern bool sde_crtc_get_fingerprint_mode(struct drm_crtc_state *crtc_state);
+extern bool sde_crtc_get_dimlayer_mode(struct drm_crtc_state *crtc_state);
 static bool
 _sde_encoder_setup_dither_for_onscreenfingerprint(struct sde_encoder_phys *phys,
 						  void *dither_cfg, int len)
@@ -3846,7 +3846,7 @@ _sde_encoder_setup_dither_for_onscreenfingerprint(struct sde_encoder_phys *phys,
 	if (!drm_enc || !drm_enc->crtc)
 		return -EFAULT;
 
-	if (!sde_crtc_get_fingerprint_mode(drm_enc->crtc->state))
+	if (!sde_crtc_get_dimlayer_mode(drm_enc->crtc->state))
 		return -EINVAL;
 
 	if (len != sizeof(dither))
@@ -4157,6 +4157,7 @@ int sde_encoder_poll_line_counts(struct drm_encoder *drm_enc)
 	return -ETIMEDOUT;
 }
 
+extern int sde_connector_update_backlight(struct drm_connector *conn);
 int sde_encoder_prepare_for_kickoff(struct drm_encoder *drm_enc,
 		struct sde_encoder_kickoff_params *params)
 {
@@ -4192,6 +4193,9 @@ int sde_encoder_prepare_for_kickoff(struct drm_encoder *drm_enc,
 				sde_enc->cur_master);
 	else
 		ln_cnt1 = -EINVAL;
+
+	if (sde_enc->cur_master)
+		sde_connector_update_backlight(sde_enc->cur_master->connector);
 
 	/* prepare for next kickoff, may include waiting on previous kickoff */
 	SDE_ATRACE_BEGIN("enc_prepare_for_kickoff");
